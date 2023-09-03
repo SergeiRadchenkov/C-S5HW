0490d44b795b168d4b8ffea33f0d9a363ad5a179
Sergei <radchenkovsa@yandex.ru>
2023-09-03 13:41:35 +0300

    Case38

--- case38/Program.cs
+++ case38/Program.cs
@@ -11,22 +11,41 @@ int InputNum (string message)
 void FillArray(double[] newArr)
 {
     Random rnd = new Random();
-    for (int i = 0; i < doubles.Length; i++)
+    for (int i = 0; i < newArr.Length; i++)
     {
-        int min = -99;
+        int min = 1;
         int max = 100;
-        doubles[i] = Math.Round(rnd.NextDouble() + rnd.Next(min, max), 2);
+        newArr[i] = Math.Round(rnd.NextDouble() + rnd.Next(min, max), 2);
     }
 }
 
 double FindDifMaxMin(double[] newArr)
 {
     double difMaxMin = 0;
-    
+    int max = 0;
+    int min = 0;
+    for (int i = 1; i < newArr.Length; i++)
+    {
+        if (newArr[i] > newArr[max])
+        max = i;
+        else if (newArr[i] < newArr[min])
+        min = i;
+    }
+    return difMaxMin = Math.Round(newArr[max] - newArr[min], 2);
+}
+
+void PrintArray(double[] newArr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < newArr.Length -1; i++)
+        Console.Write(newArr[i] + "; ");
+    Console.Write(newArr[newArr.Length -1] + " ]");
+    Console.WriteLine();
 }
 
 int size = InputNum("Введите размер массива: ");
 double[] doubles = new double[size];
 FillArray(doubles);
-
-Console.WriteLine(String.Join(" ", doubles));
+double res = FindDifMaxMin(doubles);
+PrintArray(doubles);
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {res}.");
